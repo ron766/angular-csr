daa232fa93b47cb609ff11869f150242b8988fff
ron766 <rohan.patil@contentstack.com>
2025-05-02 18:29:18 +0530

    lytics-test | Add a route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 
+import { Route2Component } from './route-2/route-2.component';
+
 export const routes: Routes = [
-    { path: 'route1', component: <div>Route 2</div> },
+    { path: 'route1', component: Route2Component },
 ];
